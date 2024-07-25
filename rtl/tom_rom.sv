99635a919eb5150a82187a6c4ec5d019ce994901
Jakub Brzazgacz <jakubbrzazgacz@gmail.com>
2024-07-25 16:44:29 +0200

    adjusted module description

--- rtl/tom_rom.sv
+++ rtl/tom_rom.sv
@@ -1,20 +1,12 @@
-//////////////////////////////////////////////////////////////////////////////
-/*
- Module name:   template_rom
- Author:        Robert Szczygiel
- Version:       1.0
- Last modified: 2017-04-03
- Coding style: Xilinx recommended + ANSI ports
- Description:  Template for ROM module as recommended by Xilinx
-
- ** This example shows the use of the Vivado rom_style attribute
- **
- ** Acceptable values are:
- ** block : Instructs the tool to infer RAMB type components.
- ** distributed : Instructs the tool to infer LUT ROMs.
- **
+/**
+ * Copyright (C) 2023  AGH University of Science and Technology
+ * MTM UEC2
+ * Author: Tomasz Maslanka, Jakub Brzazgacz
+ *
+ * Description:
+ * Module reading data from ROM
  */
-//////////////////////////////////////////////////////////////////////////////
+
  module tom_rom
 	#(parameter
 		ADDR_WIDTH = 20,
